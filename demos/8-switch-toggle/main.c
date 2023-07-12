76a8f1e4bda4438dd884061b4df919eb8d7efe59
ErikLaN68 <eriklaneave@gmail.com>
2023-07-11 19:23:37 -0600

    Kinda works

---
+++ demos/8-switch-toggle/main.c
@@ -0,0 +1,83 @@
+#include <msp430.h>
+#include "libTimer.h"
+
+#define LED_RED BIT0               // P1.0
+#define LED_GREEN BIT6             // P1.6
+#define LEDS (BIT0 | BIT6)
+
+#define SW1 BIT3		/* switch1 is p1.3 */
+#define SWITCHES SW1		/* only 1 switch on this board */
+
+void main(void) 
+{  
+  configureClocks();
+
+  P1DIR |= LEDS;
+  P1OUT &= ~LEDS;		/* leds initially off */
+  
+  P1REN |= SWITCHES;		/* enables resistors for switches */
+  P1IE |= SWITCHES;		/* enable interrupts from switches */
+  P1OUT |= SWITCHES;		/* pull-ups for switches */
+  P1DIR &= ~SWITCHES;		/* set switches' bits for input */
+
+  or_sr(0x18);  // CPU off, GIE on
+} 
+
+char pressCount = 0;
+char greenOn = 0;
+char redOn = 0;
+
+void
+switch_interrupt_handler()
+{
+  char p1val = P1IN;		/* switch is in P1 */
+/* update switch interrupt sense to detect changes from current buttons */
+  P1IES |= (p1val & SWITCHES);	/* if switch up, sense down */
+  P1IES &= (p1val | ~SWITCHES);	/* if switch down, sense up */
+  
+  if (p1val & SW1) {
+    pressCount++;
+  }
+  
+/* up=red, down=green */
+  if (pressCount == 1) {
+    greenOn = 1;
+    redOn = 1;
+  }
+  /*else if (pressCount == 2) {
+    redOn = 0;
+  }
+  else if (pressCount == 3) {
+    greenOn = 0;
+    redOn = 0;
+  }*/
+  else{
+    greenOn = 0;
+    redOn = 0;
+    pressCount = 0;
+  }
+  
+}
+
+
+/* Switch on P1 (S2) */
+void
+__interrupt_vec(PORT1_VECTOR) Port_1(){
+  if (P1IFG & SWITCHES) {	      /* did a button cause this interrupt? */
+    P1IFG &= ~SWITCHES;               /* clear pending sw interrupts */
+    switch_interrupt_handler();	/* single handler for all switches */
+  }
+  
+  if (greenOn) {
+    P1OUT |= LED_GREEN;
+  } else {
+    P1OUT &= ~LED_GREEN;
+  }
+
+  if (redOn) {
+    P1OUT |= LED_RED;
+  } else {
+    P1OUT &= ~LED_RED;
+  }
+  
+}
